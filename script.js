f41d6f43d7f8eaba779b4649af12a642d353d968
Amit Paul <pamit7117@gmail.com>
2020-03-04 00:43:32 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#DAF7A6",
+    "#FFC300",
+    "#FF5733",
+    "#C70039",
+    "#900C3F",
+    "#581845"
+  ];
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
+
+  const createBubble = index => {
+    //Create bubbles
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
